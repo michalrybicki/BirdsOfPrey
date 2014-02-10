5db6da827c437976de226d116c047e33f5384f0f
Michal Rybicki <michalr@inbox.com>
2014-02-10 21:45:12 +0100

    GroupLayout for Buttons.

--- src/mainPack/Game.java
+++ src/mainPack/Game.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class Game extends JFrame implements ActionListener,WindowListener
 {
@@ -13,34 +16,82 @@ public class Game extends JFrame implements ActionListener,WindowListener
 	private JButton startButton = new JButton("Start");
 	private JButton quitButton = new JButton("Quit");
 	private JButton pauseButton = new JButton("Pause");
+	private JRadioButton serverButton = new JRadioButton("Server");
+	private JRadioButton clientButton = new JRadioButton("Client");
+	private JTextField ipAddress = new JTextField("192.168.1.100", 10);
+  
 	private boolean running = false;
 	private boolean paused = false;
 	private int fps = 0;
 	 
+	 
 	public Game()
 	{
 		super("SpaceCraft T2");
-		setSize(800, 480);
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.setLayout(new GridLayout());
-		buttonPanel.add(startButton);
-		buttonPanel.add(pauseButton);
-		buttonPanel.add(quitButton);
+		setSize(800, 600);
+		
+		//Get Frame container
 		Container container = getContentPane();
-		container.setLayout(new BorderLayout());
+		container.setLayout(new BorderLayout()); 
+		
+		//Set settingsPanel
+		JPanel settingsPanel = new JPanel();
+		GroupLayout layout = new GroupLayout(settingsPanel);
+		settingsPanel.setLayout(layout);
+		layout.setAutoCreateGaps(true);
+		layout.setAutoCreateContainerGaps(true);
+		
+		serverButton.setSelected(true);
+		ipAddress.setEditable(false);
+		ButtonGroup group = new ButtonGroup();
+		group.add(serverButton);
+		group.add(clientButton);
+		
+		//Set GroupLayout
+		layout.setAutoCreateGaps(true);
+		layout.setAutoCreateContainerGaps(true);
+
+		layout.setHorizontalGroup(layout.createSequentialGroup()
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+						.addComponent(startButton)
+						.addComponent(serverButton)
+						.addComponent(clientButton))
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+						.addComponent(pauseButton)
+						.addComponent(ipAddress ))
+				.addComponent(quitButton)      
+		);
+		layout.setVerticalGroup( layout.createSequentialGroup()
+		      .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+		           .addComponent(startButton)
+		           .addComponent(pauseButton)
+				   .addComponent(quitButton)) 
+		      .addComponent(serverButton)
+		      .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+		           .addComponent(ipAddress)
+		      .addComponent(clientButton))
+		      
+		);
+		
+
+		layout.linkSize(SwingConstants.HORIZONTAL,startButton,pauseButton, quitButton, ipAddress );
+		
 		container.add(gamePanel, BorderLayout.CENTER);
-		container.add(buttonPanel, BorderLayout.SOUTH);
+		container.add(settingsPanel, BorderLayout.PAGE_END);
+		
 		
 		//Add Listeners
         addWindowListener(this); 
 
+        startButton.addActionListener(this);
+		pauseButton.addActionListener(this);
+		quitButton.addActionListener(this);
+		serverButton.addActionListener(this);
+		clientButton.addActionListener(this);
+		ipAddress.addActionListener(this);
+		
 		setFocusable(true);
 		setVisible(true);
-
-        startButton.addActionListener(this);
-		quitButton.addActionListener(this);
-		pauseButton.addActionListener(this);
-
 	}
 
 	public static void main(String[] args)
@@ -58,11 +109,51 @@ public class Game extends JFrame implements ActionListener,WindowListener
 			if (running)
 			{
 				startButton.setText("Stop");
+				ipAddress.setEditable(false);
+				clientButton.setEnabled(false);
+				serverButton.setEnabled(false);
+				
+				if (clientButton.isSelected()) 
+				{
+					if(validateIPAddress(ipAddress.getText()))
+					{	
+						Color color= Color.BLACK; 
+						ipAddress.setForeground(color);
+					
+						if(isIPReachable(ipAddress.getText()))
+						{		
+							System.out.println("Found IP.");
+							System.out.println("Connecting to " + ipAddress.getText() );
+						}
+						else
+						{
+							System.out.println("IP not found.");
+						}
+					}
+					else
+					{
+						Color color= Color.RED; 
+						ipAddress.setForeground(color); 
+						System.out.println("IP not correct ");
+					}
+				}	
+					
 				runGameLoop();
 			}
 			else
 			{
 				startButton.setText("Start");
+				if (serverButton.isSelected()) 
+				{
+					ipAddress.setEditable(false);
+				}
+				else
+				{
+					ipAddress.setEditable(true);
+				}
+				clientButton.setEnabled(true);
+				serverButton.setEnabled(true);
+				System.out.println("Dissconected");
 			}
 		}
 		else if (s == pauseButton)
@@ -80,8 +171,33 @@ public class Game extends JFrame implements ActionListener,WindowListener
 		else if (s == quitButton)
 		{
 			System.exit(0);
+		}		
+		else if (s == clientButton)
+		{	
+			ipAddress.setEditable(true);
+ 
+		}
+		else if (s == serverButton)
+		{
+			ipAddress.setEditable(false);
 		}
 		
+		else if (s == ipAddress)
+		{
+			if(validateIPAddress(ipAddress.getText()))
+			{
+				Color color = Color.BLACK;			    
+				ipAddress.setForeground(color);  
+				 
+			}
+			else
+			{
+				Color color= Color.RED; 
+				ipAddress.setForeground(color); 
+			}
+			
+			System.out.println("List txt");
+		}
 		//Set focus on GAME
 		gamePanel.requestFocus();
 	}
@@ -93,8 +209,7 @@ public class Game extends JFrame implements ActionListener,WindowListener
 		{
 			public void run()
 			{
-				gameLoop();	       
-
+				gameLoop();
 			}
 		};
 		loop.start();
@@ -179,6 +294,53 @@ public class Game extends JFrame implements ActionListener,WindowListener
 		gamePanel.repaint();
 	}
 
+	public boolean validateIPAddress(String ipAddress)
+	{
+		String[] octets = ipAddress.split("\\.");
+		if(octets.length != 4)
+		{
+			return false;
+		}
+		for (String str : octets)
+		{
+			int i;
+			try
+			{
+				i = Integer.parseInt(str);
+			}
+			catch(NumberFormatException ex)
+			{ 
+				  i=-1;
+			}
+			
+			if ((i<0) || (i>255))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public boolean isIPReachable(String ipAdd)
+	{
+		boolean reachable = false;
+
+	    try {		
+			InetAddress inet1 = InetAddress.getByName(ipAdd);
+			reachable = inet1.isReachable(5000);
+	    } catch (UnknownHostException e) 
+	    {
+	    	 
+		} catch (IOException e){
+			 
+				
+		}
+		 
+		return reachable;
+	}
+	
+	
+	
 	@Override
 	public void windowOpened(WindowEvent e) {
 		System.out.println("windowOpened");
@@ -220,6 +382,7 @@ public class Game extends JFrame implements ActionListener,WindowListener
 	public void windowDeactivated(WindowEvent e) {
 		System.out.println("windowDeactivated");
 	}
+
 
 
 
